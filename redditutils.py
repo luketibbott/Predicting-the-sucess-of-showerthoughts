8df274760a371406d5dc0a5e4fd02590708ff43d
luketibbott <luke.tibbott@gmail.com>
2018-11-13 10:37:01 -0800

    Add return statement to remove bad indices

--- redditutils.py
+++ redditutils.py
@@ -7,7 +7,7 @@ import numpy as np
 import re
 
 from string import punctuation
-from imblearn import SMOTE as sm
+from imblearn.over_sampling import SMOTE
 
 def lower_string(s):
     return s.lower()
@@ -26,12 +26,35 @@ def make_labels(score):
 
 def remove_bad_indices(features, response):
     # Remove document-response pairs that failed to be embedded in vector space
-    bad_indices = list(np.unique(np.where(np.isnan(features, axis=0)[0])))
-    
+    bad_indices = list(np.unique(np.where(np.isnan(features))[0]))
+        
     features = np.delete(features, bad_indices, axis=0)
     response = np.delete(np.array(response), bad_indices)
+    
+    return features, response
 
 def upsample(features, response):
     # Returns SMOTE-upsample features and response
 
+    sm = SMOTE()
+
     return sm.fit_sample(features, response)
+
+def make_w2v_features(words, model, num_features):
+    features = np.zeros(num_features)
+    
+    model_vocab = set(model.index2word)
+    
+    num_words = 0
+    
+    # Loop over words in documents. If the word is in model's vocabulary,
+    # generate its feature vector
+    for w in words:
+        if w in model_vocab:
+            num_words += 1
+            features = np.add(features, model[w])
+            
+    # Normalize the feature vector
+    features = np.divide(features, num_words)
+    
+    return features
